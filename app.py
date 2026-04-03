15c16cba37ca2f7719bf1c264d897cc73dadf9de
Antonio sa <antonim86web@gmail.com>
2026-04-03 20:24:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def genera_relazione_ia(p_id, p_nome, giorni=30):
     {testo_per_ia}
     """
     try:
-                # Assicurati che ci sia 'v1' nel nome del modello se il 404 persiste
+         # Assicurati che ci sia 'v1' nel nome del modello se il 404 persiste
 model = genai.GenerativeModel('models/gemini-1.5-flash')
 
 
