6d90f6693eff741d1baf24fa6eb5c2c8cae5452f
Antonio sa <antonim86web@gmail.com>
2026-04-03 17:27:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 
 # --- CONFIGURAZIONE IA (INSERISCI QUI LA TUA API KEY) ---
 # Ottienila su: https://aistudio.google.com/app/apikey
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"], transport='rest')
 
 
 # --- FUNZIONE AGGIORNAMENTO DB (INTEGRALE) ---
