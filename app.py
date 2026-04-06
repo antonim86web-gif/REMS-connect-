345c8081731c801aa72d8f19f92a749e343fd39b
Antonio sa <antonim86web@gmail.com>
2026-04-07 00:52:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -322,7 +322,7 @@ elif nav == "📊 Monitoraggio":
             
             col1, col2 = st.columns([4, 1])
             with col2:
-                eventi = db_run("SELECT data, op, nota FROM eventi WHERE id=? ORDER BY id_u DESC", (p_id,))
+              if  eventi = db_run("SELECT data, op, nota FROM eventi WHERE id=? ORDER BY id_u DESC", (p_id,))
                     pdf_data = genera_pdf_clinico(nome, eventi)
                     st.download_button(f"📥 Scarica PDF", data=pdf_data, file_name=f"diario_{nome}.pdf", mime="application/pdf", key=f"pdf_{pid}")
             
